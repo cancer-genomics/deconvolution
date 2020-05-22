675b2db60379e5a162b33b1510999e5ef59f1687
rscharpf <rscharpf@jhu.edu>
2020-05-22 14:01:36 -0400

    RS added new jags model

--- code/simulation.R
+++ code/simulation.R
@@ -14,7 +14,6 @@ normal <- rpois(n, lambdas[2])
 tumor.fraction <- 0.99
 means <- tumor.fraction*normal + (1-tumor.fraction)*wbc
 y <- rpois(n, means)
-
 data <- list(y=y, wbc=log(wbc), normal=log(normal))
 
 fit <- jags.model("sim_mixmodel.jag",
@@ -25,11 +24,29 @@ samples <- coda.samples(fit, variable.names=c("b1", "b0"),
                         n.iter=10000,
                         thin=10)
 ##samples2 <- samples %>% filter(Parameter %in% c('b1[1]','b1[2]'))
-samples2 <- ggs(samples, family="b1")
+samples2 <- ggs(samples, family="b1") %>%
+    filter(Parameter=="b1[1]")
 ggs_traceplot(samples2) +
-    ylim(c(0, 1))  +
+    ylim(c(0.5, 1))  +
     theme_bw() +
-    ylab("")
+    ylab("") +
+    geom_hline(yintercept=0.99)
+##
+## Next steps
+## 
+## - What happens with wbc contribution is 1 and normal is zero *
+##
+## - 50000 bins
+## - 500 groups of 100
+## - 10% of the groups are .95 and .05, rest are 1 and 0
+##
+## proportion of group that have a contribution from the tumor
+##
+## log(theta) = b0 + (b1 * wbc + (1-b1)*tumor)*z + (b1 * wbc)(1-z)
+## z ~ dbin(pi, 1) ##
+## pi ~ beta(1, 1)
+
+
 
 ##m <- spread(samples2, Parameter, value)
 ##coda::effectiveSize(m[[3]])
